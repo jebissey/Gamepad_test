d166963cc8a47a08bb7f3b256ad48b242b0ae6c0
jeb <je.bissey@gmail.com>
2024-01-02 11:08:41 +0100

    First commit

---
+++ Gamepad_test/MainWindowView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Gamepad_test;
+
+public partial class MainWindowView : Window
+{
+    public MainWindowView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Gamepad_test/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Gamepad;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Gamepad_test;
+
+internal class MainWindowViewModel : ObservableObject
+{
+    private GamePad gamePad = new();
+
+    public MainWindowViewModel()
+    {
+        if (!gamePad.StartListening())
+        {
+            MessageBox.Show("No gamepad found");
+        }
+    }
+
+    public ICommand WindowClosing => new RelayCommand<CancelEventArgs>((args) =>
+    {
+        GamePad.StopListening();
+    });
+}
