87cfef28e3fdc3ea8ff5f362d994517641813555
jeb <je.bissey@gmail.com>
2024-01-03 19:20:41 +0100

    Added 4 axes

--- Gamepad_test/MainWindowViewModel.cs
+++ Gamepad_test/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
 using Gamepad;
 using System.ComponentModel;
 using System.Windows;
@@ -11,8 +12,23 @@ internal class MainWindowViewModel : ObservableObject
 {
     private GamePad gamePad = new();
 
+    public int Axis1 => GamePad.GetAxes(0) / 64;
+    public int Axis2 => GamePad.GetAxes(1) / 64;
+    public int Axis3 => GamePad.GetAxes(2) / 64;
+    public int Axis4 => GamePad.GetAxes(3) / 64;
+
+
     public MainWindowViewModel()
     {
+        WeakReferenceMessenger.Default.Register<short[]>(this, (_, axes) =>
+        {
+            OnPropertyChanged(nameof(Axis1));
+            OnPropertyChanged(nameof(Axis2));
+            OnPropertyChanged(nameof(Axis3));
+            OnPropertyChanged(nameof(Axis4));
+        });
+
+
         if (!gamePad.StartListening())
         {
             MessageBox.Show("No gamepad found");
