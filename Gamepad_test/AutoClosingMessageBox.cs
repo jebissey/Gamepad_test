b76a150f5b75aae2b55cff016c84a3d4c5b2ed8f
jeb <je.bissey@gmail.com>
2024-01-05 13:50:30 +0100

    cosmetic

--- Gamepad_test/AutoClosingMessageBox.cs
+++ Gamepad_test/AutoClosingMessageBox.cs
@@ -4,6 +4,7 @@ namespace Gamepad_test;
 
 public class AutoClosingMessageBox
 {
+    const int WM_CLOSE = 0x0010;
     Timer _timeoutTimer;
     string _caption;
     
@@ -26,12 +27,11 @@ public class AutoClosingMessageBox
         _timeoutTimer.Dispose();
     }
 
-    const int WM_CLOSE = 0x0010;
 
     [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
     static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
 
     [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
-    static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+    static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
 }
